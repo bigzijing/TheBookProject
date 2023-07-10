73ab2b257ae28e0bb8686ae062937f828abcecaa
Zi Jing <bigzijing@gmail.com>
2023-07-11 00:33:45 +0800

    Implement a working DBO

--- build.sbt
+++ build.sbt
@@ -5,5 +5,12 @@ libraryDependencies ++= Seq(
   "dev.zio" %% "zio" % "2.0.2",
   "com.github.ghostdogpr" %% "caliban" % "2.0.2",
   "com.github.ghostdogpr" %% "caliban-zio-http" % "2.0.2",
-  "io.d11" %% "zhttp" % "2.0.0-RC10"
+  "io.d11" %% "zhttp" % "2.0.0-RC10",
+  "com.leadiq" %% "reactivemongo-zio" % "2.0.0-RC6",
+  "dev.zio" %% "zio-logging" % "2.1.13",
+  "dev.zio" %% "zio-logging-slf4j" % "2.1.13",
+  "dev.zio" %% "zio-logging-slf4j-bridge" % "2.1.13",
+  "com.beachape" %% "enumeratum" % "1.7.2",
+  "com.github.tototoshi" %% "scala-csv" % "1.3.10",
+  "io.scalaland" %% "chimney" % "0.7.5"
 )
---
+++ src/main/scala/com/zij/tbp/services/mongo/models/BookDBO.scala
@@ -0,0 +1,49 @@
+package com.zij.tbp.services.mongo.models
+
+import com.zij.tbp.models.{ Book, CoverType, Language }
+import reactivemongo.api.bson.{ BSONDocumentReader, BSONDocumentWriter, BSONObjectID, Macros }
+import io.scalaland.chimney.dsl._
+
+import java.time.Instant
+
+case class BookDBO(
+  _id: Option[BSONObjectID] = None,
+  createdDate: Option[Instant] = None,
+  title: String,
+  isbn: String,
+  author: String,
+  publisher: String,
+  category: List[String],
+  goodreadsCategory: List[String],
+  language: List[String],
+  coverType: String,
+  pages: Option[Int] = None,
+  condition: Option[Double] = None,
+  rating: Option[Double] = None,
+  goodreadsRating: Option[Double] = None,
+  location: Option[String] = None,
+  status: Option[String] = None,
+  read: Boolean = false,
+  dateBought: Option[Instant] = None,
+  notes: Option[String] = None
+) {
+
+  def toShortString     = s"$title by $author"
+  def toFormattedString =
+    s"""Title: $title
+       |ISBN: $isbn
+       |Author: $author
+       |""".stripMargin
+}
+
+object BookDBO {
+
+  def fromBook(book: Book)                             =
+    book
+      .into[BookDBO]
+      .withFieldComputed(_.language, _.language.map(_.entryName))
+      .withFieldComputed(_.coverType, _.coverType.entryName)
+      .transform
+  implicit def bookWriter: BSONDocumentWriter[BookDBO] = Macros.writer[BookDBO]
+  implicit def bookReader: BSONDocumentReader[BookDBO] = Macros.reader[BookDBO]
+}

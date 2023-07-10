66bdc0a07e667e063ca6c0eee83f56e797652274
Zi Jing <bigzijing@gmail.com>
2023-07-11 00:34:36 +0800

    Implement a script/app that imports from GSheets into Mongo

---
+++ src/main/scala/com/zij/tbp/ImportFromGSheets.scala
@@ -0,0 +1,71 @@
+package com.zij.tbp
+
+import com.github.tototoshi.csv.CSVReader
+import com.zij.tbp.services.mongo.models.BookDBO
+import zio.{ ZIOAppDefault, ZLayer }
+import reactivemongo.zio._
+import zio.Console.{ printLine, readLine }
+import zio.stream.ZStream
+import cats.implicits._
+import com.zij.tbp.models.{ CoverType, Language }
+import com.zij.tbp.models.Book
+
+import java.time.Instant
+import scala.io.Source
+import scala.util.Try
+
+object ImportFromGSheets extends ZIOAppDefault {
+  def stringToBoolean(s: String): Boolean = s match {
+    case "1" => true
+    case _   => false
+  }
+
+  def stringToOption(s: String): Option[String] = s match {
+    case "" => none
+    case s  => s.some
+  }
+
+  def getLanguages(s: String): List[Language] = s.trim.split(",").toList.flatMap(Language.withNameInsensitiveOption)
+
+  def readFromCsv(fileName: String): ZStream[Any, Nothing, BookDBO] = ZStream
+    .fromIterable(
+      CSVReader
+        .open(Source.fromResource(fileName))
+        .toLazyList
+        .drop(2)
+    )
+    .map { line =>
+      BookDBO.fromBook(
+        Book(
+          _id = none,
+          createdDate = Instant.now.some,
+          title = line.head,
+          isbn = line(10),
+          author = line(1),
+          publisher = line(2),
+          category = List(line(3)),
+          goodreadsCategory = List.empty,
+          language = getLanguages(line(4)),
+          coverType = CoverType.gsheetsToEnumRouter(line(5)),
+          pages = stringToOption(line(6)).flatMap(s => Try(s.toInt).toOption),
+          condition = stringToOption(line(7)).flatMap(s => Try(s.toDouble).toOption),
+          rating = stringToOption(line(8)).flatMap(p => Try(p.toDouble).toOption),
+          goodreadsRating = stringToOption(line(9)).flatMap(p => Try(p.toDouble).toOption),
+          location = stringToOption(line(11)),
+          status = stringToOption(line(12)),
+          read = stringToBoolean(line(14)),
+          dateBought = none,
+          notes = stringToOption(line(13))
+        )
+      )
+    }
+
+  val run = (for {
+    filename        <- getFilename
+    booksCollection <- Mongo.collection(CollectionName("book"))
+    _               <- booksCollection {
+                         readFromCsv(filename).runForeach(Mongo.insert.one(_))
+                       }
+  } yield ())
+    .provide(getMongoFromCredentials)
+}
---
+++ src/main/scala/com/zij/tbp/package.scala
@@ -0,0 +1,25 @@
+package com.zij
+
+import reactivemongo.zio.Mongo
+import zio.Console.{printLine, readLine}
+import zio.{ZIO, ZLayer}
+
+import java.io.IOException
+
+package object tbp {
+
+  def getMongoFromCredentials: ZLayer[Any, Throwable, Mongo] =
+    ZLayer
+      .fromZIO(
+        for {
+          username <- printLine("Enter URL encoded Mongo username") *> readLine
+          password <- printLine("Enter URL encoded Mongo password") *> readLine
+        } yield s"mongodb+srv://$username:$password@the-book-project-cluste.oglrs3m.mongodb.net/the-book-project?retryWrites=true&w=majority"
+      )
+      .flatMap(s => Mongo.fromConnectionString(s.get))
+
+  def getFilename: ZIO[Any, IOException, String] =
+    for {
+      filename <- printLine("Enter filename in resources folder") *> readLine
+    } yield filename
+}
